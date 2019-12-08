8594e2238ddcc89b428c7e50b8c8f623520fc1b5
Ryan C. Durham <ryan@stagerightlabs.com>
2019-12-08 14:02:29 -0800

    Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <div v-if="loaded">
+      <h2>{{ dataSetName }}</h2>
+      <h3>https://catalog.data.gov/dataset/pm2-5-air-quality-standard-update</h3>
+      <div v-for="complaint in filteredData" :key="complaint[1]">
+        <p style="margin-bottom: 20px; text-align:justify">
+          {{ complaint[13] }}
+          <br />
+          - {{ complaint[8] }} {{ complaint[9] }}
+        </p>
+      </div>
+    </div>
+    <p v-else>Loading...</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      airData: {},
+      loaded: false
+    }
+  },
+  computed: {
+    filteredData () {
+      return this.airData.data.filter(d => d[13])
+    },
+    dataSetName () {
+      return this.airData.meta.view.name
+    }
+  },
+  methods: {
+    getJsonData () {
+      fetch('air_quality.json')
+        .then(response => response.json())
+        .then(data => {
+          this.airData = data
+          this.loaded = true
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  mounted () {
+    this.getJsonData()
+  }
+}
+</script>
